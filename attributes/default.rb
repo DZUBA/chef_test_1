43e9880cc961e6be6e886e28ccebcb01c5f06b92
DZUBA <zzzdzuba@gmail.com>
2016-11-11 13:44:22 +0200

    Adding attributes

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook Name:: chef_task_1
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+default['chef_task_1']['user'] = 'devops'
+default['chef_task_1']['group'] = 'devops'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,25 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# create user "devops"
+user node['chef_task_1']['user'] do
+  comment 'devops user'
+  home '/home/devops'
+  shell '/bin/bash'
+  action :create
+end
+
+# create group "devops"
+group node['chef_task_1']['group'] do
+  action :create
+  members node['chef_task_1']['user']
+end
+
+# create file
+file '/opt/hello' do
+  content 'Hello DevOps!'
+  mode '0664'
+  owner node['chef_task_1']['user']
+  group node['chef_task_1']['group']
+end
